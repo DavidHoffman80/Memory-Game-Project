d3f9c065e2ffa3f0b6f3e8f87358a7e596f07b41
Dave Hoffman <spaghettidave80@gmail.com>
2018-05-23 11:36:53 -0400

    Fully working project

--- js.js
+++ js.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
+  // Hide the modal
+  let modal = document.getElementById('modal');
+  modal.style.display = 'none';
+
   // Shuffle the cards function
   function shuffleCards() {
     let cardsArray = [
@@ -29,12 +33,13 @@ document.addEventListener('DOMContentLoaded', function(){
     return cardsArray;
   }
 
+  let myTimer;
+  let seconds = 0;
+  let minutes = 0;
   function timer(){
     let displaySeconds = document.querySelector('.seconds');
     let displayMinutes = document.querySelector('.minutes');
-    let seconds = 0;
-    let minutes = 0;
-    setInterval(function(){
+    myTimer = setInterval(function(){
       if(seconds < 59){
         seconds++;
         displaySeconds.textContent = seconds;
@@ -52,29 +57,145 @@ document.addEventListener('DOMContentLoaded', function(){
     moves++;
   }
 
-  
+  function finalStarsCreator(){
+    
+  }
 
-// e.target.nextElementSibling.children[0].classList[1]
+  function youWon(){
+    clearInterval(myTimer);
+    let finalMoves = document.querySelector('.total-moves-count');
+    let finalMinutes = document.querySelector('.total-time-count-min');
+    let finalSeconds = document.querySelector('.total-time-count-sec');
+    let closeBtn = document.querySelector('.close');
+    window.setTimeout(function(){
+      modal.style.display = '';
+      closeBtn.style.cursor = 'pointer';
+      finalMoves.textContent = turn;
+      finalMinutes.textContent = minutes;
+      finalSeconds.textContent = seconds;
+      let closeModal = document.querySelector('.close');
+      closeModal.addEventListener('click', function(e){
+        modal.style.display = 'none';
+        startGame();
+      });
+    }, 500);
+    let finalStars = document.querySelector('.final-stars');
+    let starsHTML = "";
+    for(let i = stars; i > 0; i--){
+      starsHTML += '<i class="fas fa-star"></i>';
+    }
+    finalStars.innerHTML = starsHTML;
+  }
+
+  let card_one;
+  let card_two;
+  let matches = 0;
+  let turn = 0;
+  let stars = 5;
+
+  function starsStructure(){
+    let starsStructureHTML = "";
+    for(let i = stars; i > 0; i--){
+      starsStructureHTML += '<i class="fas fa-star"></i>';
+    }
+    starsPlacement.innerHTML = starsStructureHTML;
+  }
+
+  let starsPlacement = document.querySelector('.star-counter');
+  function starsTracker(){
+    
+    if(turn <= 9){
+      stars = 5;
+      starsStructure();
+    } else if(turn > 9 && turn <= 12){
+      stars = 4;
+      starsStructure();
+    } else if(turn > 12 && turn <= 14){
+      stars = 3;
+      starsStructure();
+    } else if(turn > 14 && turn <= 17){
+      stars = 2;
+      starsStructure();
+    } else{
+      stars = 1;
+      starsStructure();
+    }
+  }
+
+  function updateMoves(){
+    turn++;
+    let movesCounter = document.querySelector('.moves-count');
+    movesCounter.textContent = turn;
+    starsTracker();
+  }
+
+  function isMatched(e){    
+    if(e.target.offsetParent.classList[3] === 'card-1'){
+      card_one = e.target.nextElementSibling.children[0].classList[1];
+    } else if(e.target.offsetParent.classList[3] === 'card-2'){
+      card_two = e.target.nextElementSibling.children[0].classList[1];
+      if(card_one == card_two){
+        matches++;
+        document.querySelector('.card-1').children[1].classList.add('matched');
+        document.querySelector('.card-2').children[1].classList.add('matched');
+        // document.querySelector('.card-1').classList.add('matched');
+        // document.querySelector('.card-2').classList.add('matched');
+        document.querySelector('.card-1').classList.remove('card-1');       
+        document.querySelector('.card-2').classList.remove('card-2');
+        if(matches === 8){
+          youWon();
+        }
+      } else{
+        document.querySelector('.card-1').children[1].classList.add('no-match');
+        document.querySelector('.card-2').children[1].classList.add('no-match');
+        window.setTimeout(function(){
+          document.querySelector('.card-1').children[1].classList.remove('no-match');
+          document.querySelector('.card-2').children[1].classList.remove('no-match');
+          document.querySelector('.card-1').classList.remove('flipped', 'card-1');
+          document.querySelector('.card-2').classList.remove('flipped', 'card-2');
+        }, 1000);
+      }
+    }
+  }
+
   function flipCards(e){
-    if(moves === 1){
-      timer();
-      movesTracker();
-      e.target.offsetParent.classList.add('flipped', 'card-1');
-    } else if(moves % 2 === 0){
-      movesTracker();
-      e.target.offsetParent.classList.add('flipped', 'card-2');
-      window.setTimeout(function(){
-        document.querySelector('.card-1').classList.remove('flipped', 'card-1');
-        document.querySelector('.card-2').classList.remove('flipped', 'card-2');
-      }, 1500);
-    } else {
-      movesTracker();
-      e.target.offsetParent.classList.add('flipped', 'card-1');
-    } 
+
+    if(e.target.className === 'card-back'){
+      if(moves === 1){
+        timer();
+        movesTracker();
+        e.target.offsetParent.classList.add('flipped', 'card-1');
+        isMatched(e);
+      } else if(moves % 2 === 0){
+        movesTracker();
+        e.target.offsetParent.classList.add('flipped', 'card-2');
+        isMatched(e);
+        updateMoves();
+      } else {
+        movesTracker();
+        e.target.offsetParent.classList.add('flipped', 'card-1');
+        isMatched(e);
+      }
+    }
+  }
+
+  function startOver(){
+    clearInterval(myTimer);
+    seconds = 0;
+    minutes = 0;
+    moves = 1;
+    matches = 0;
+    turn = 0;
+    stars = 5;
+    document.querySelector('.seconds').textContent = seconds;
+    document.querySelector('.minutes').textContent = minutes;
+    document.querySelector('.moves-count').textContent = turn;
+    starsStructure();
   }
 
   function startGame(){
-    startGameButton.textContent = "Start Over";
+    startOver();
+    // startGameButton.textContent = "Start Over";
     const randomCards = shuffleCards();
     const cardsArr = randomCards.slice();
     let card = document.querySelectorAll('.card');
